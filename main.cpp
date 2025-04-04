1f96083daf7cad6b5ccd7d32a99cc39807b72891
Vincent Tran <33761211+VincentVinhTran@users.noreply.github.com>
2025-04-04 11:47:44 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
-#include <cstdlib> 
+#include <cstdlib>
 #include <ctime>
 using namespace std;
 
-// ** REQUIRED FUNCTIONS **
-char userChoice();
-char computerChoice();
-void determineWinner(char, char);
-bool playAgain();
-// ************************
+// Game Settings
+const int MAX_ATTEMPTS = 5;
+const int NUM_CANOES = 3;
+const int MAX_CANOE_SIZE = 4;
+// Board Size
+const int ROWS = 4;
+const int COLS = 4;
+// Display Symbols
+const char UNKNOWN = '#';
+const char MISS = '*';
+
+
+/***** Function Prototypes (Optional) *****/
+/* Fills 2D array with provided char */
+// void fillBoard(char[][COLS], char);
+
+// Prints 2D array to the terminal
+// void displayBoard(char[][COLS]);
+
+/* Checks if a valid row/col selection is within
+   bounds of the 2D array */
+// bool validChoice(int, int);
+
+/* Checks if a row/col selection matches the
+   provided char in the 2D array */
+// bool match(char[][COLS], int x, int y, char);
+
+/* Adds a randomly generated canoe to the 2D array */
+// void addCanoe(char[][COLS], char);
+
+/* Checks if all canoes have been found by comparing
+   two 2D arrays */
+// bool checkWinner(char[][COLS], char[][COLS]);
+
 
 int main() {
   return 0;
